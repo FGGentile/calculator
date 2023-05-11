4ad8f3a4098f2f29905967a74d48696aad22c82b
FGGentile <114667938+FGGentile@users.noreply.github.com>
2023-05-10 23:35:36 -0300

    Add files via upload

---
+++ function calculate() {.js
@@ -0,0 +1,6 @@
+function calculate() {
+	var inputValue = document.getElementById("inputValue").value;
+	var fivePercent = inputValue * 0.05;
+	var result = fivePercent * 1.1;
+	document.getElementById("result").innerHTML = "Result: " + result;
+}
